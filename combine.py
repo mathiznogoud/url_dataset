9e9f46d35d9af538794fb0798b1f2fb9277f227c
mathiznogoud <mathiznogoud@gmail.com>
2020-06-03 09:44:39 +0700

    add new combine method

---
+++ combine.py
@@ -0,0 +1,16 @@
+import os
+import glob
+import pandas as pd
+
+os.chdir("features/")
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+for i in all_filenames:
+    data = []
+    with open(i,'r') as f:
+        data = f.readlines()
+    with open('../combined_dataset.arff','a') as f:
+        for j in data:
+            f.write(j)
